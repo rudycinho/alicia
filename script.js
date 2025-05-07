45e990942613c3e81d26059603d5358afb0449e3
Rudycinho <rudycinho72@gmail.com>
2025-05-06 23:40:05 -0400

    commit mediocre

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle Navigation Menu
+const navToggle = document.getElementById('nav-toggle');
+const navMenu = document.getElementById('nav-menu');
+
+navToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
